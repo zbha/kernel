4fa214571deaa32bc62b0cb9eb579c9510e3e252
zbha <75596341+zbha@users.noreply.github.com>
2021-01-20 16:58:47 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+cd /tmp/ 
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4-wily/linux-headers-4.4.0-040400_4.4.0-040400.201601101930_all.deb 
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4-wily/linux-headers-4.4.0-040400-generic_4.4.0-040400.201601101930_i386.deb 
+Wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4-wily/linux-image-4.4.0-040400-generic_4.4.0-040400.201601101930_i386.deb 
+sudo dpkg -i *.deb
